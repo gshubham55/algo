5f3b617708fde40d4efc598c2cb8e551760fdced
Shubham Gupta <gshubham55@gmail.com>
2013-02-01 23:26:29 +0530

    3rd

---
+++ bubblesort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+void bubble(int a[],int n)
+{
+     for ( int i=0;i<n;i++)
+     {
+         for ( int j=0;j<n-i-1;j++)
+         {
+             if(a[j]>a[j+1])
+            {
+                 int temp=a[j];
+                 a[j]=a[j+1];
+                 a[j+1]=temp;
+            }
+         }
+     }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for ( int i=0;i<n;i++) cin>>a[i];
+    bubble(a,n);
+    for ( int i=0;i<n;i++) cout<<a[i]<<endl;
+ //   system("pause");
+}
